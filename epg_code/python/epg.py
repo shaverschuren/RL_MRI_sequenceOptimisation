5c3a7b60fbff36bbb0368677944a66cdc03471f2
shaverschuren <s.h.a.verschuren@student.tue.nl>
2021-09-09 11:11:59 +0200

    Work on Python EPG reimplementation - Several bugfixes - Plotting + example implemented
    Code runs now, still not able to reproduce MATLAB results though.

--- epg_code/python/epg.py
+++ epg_code/python/epg.py
@@ -8,6 +8,7 @@ by Sjors Verschuren in 09/2021."""
 
 from typing import Union, Tuple
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def epg(
@@ -39,7 +40,7 @@ def epg(
                                            -1  - Alternating RF flip angles
                                            0   - For all pulses, phi=0deg
             rf_phase_inc  (float)        : Linear RF phase increment,
-                                           used only if rf_phase_mode == +1 
+                                           used only if rf_phase_mode == +1
                                            (default 150) [deg]
 
         Returns:
@@ -57,16 +58,31 @@ def epg(
     # Determine effective alpha and phi arrays and dedicated
     # length parameters: SSFP variant settings
     if type(alpha) == np.ndarray:
-        phi = [-np.angle(SP)] * len(alpha)
-        fa = [abs(SP) * alpha] * len(alpha)
+        # Calculate phi, flip angle array
+        phi = np.array([-np.angle(SP)] * len(alpha))
+        fa = np.array([abs(SP) * alpha] * len(alpha))
+        # The flip angle (array) is repeated N_in times
+        for pn in range(1, N_in):
+            fa = np.concatenate(fa, alpha)
     elif type(alpha) == float:
-        phi = -np.angle(SP)
-        fa = abs(SP) * alpha
+        # Calculate phi, flip angle array
+        phi = np.array(-np.angle(SP))
+        fa = np.array(abs(SP) * alpha)
+        # The flip angle is repeated N_in times
+        fa_arr = np.zeros((N_in))
+        fa_arr[0] = fa
+        for pn in range(1, N_in):
+            fa_arr[pn] = alpha
+        fa = fa_arr
     else:
-        raise TypeError("alpha should be either a list or float!")
+        raise TypeError("alpha should be either an array or float!")
 
-    for pn in range(2, N_in):  # The flip angle (array) is repeated N_in times
-        fa = [fa, alpha]
+    # fa_arr = np.zeros((1, len(alpha) * N_in))
+    # fa_arr[:len(alpha)] = fa
+    # for pn in range(1, N_in):  # The flip angle (array) is repeated N_in times
+    #     fa_arr[len(alpha) * pn:len(alpha) * (pn + 1)] = np.array(alpha)
+
+    # fa = fa_arr
 
     N = len(fa)
     Nm1 = N - 1
@@ -79,13 +95,13 @@ def epg(
                 fa[i] = -fa[i]
 
         phi[:N] = 0
-        if (N > 1):
+        if N > 1:
             # More than one alternating flip angle? Use initial fa/2 pulse!
             pass  # fa(1)=fa(1)/2;
 
     elif rf_phase_mode == +1:
         # Realize RF spoiling with dedicated RF phase angle
-        pn = np.array(range(N))
+        pn = np.array(range(N)) + 1
         phi = (pn - 1) * pn / 2.0 * rf_phase_inc / 180.0 * np.pi
 
     elif rf_phase_mode == 0:
@@ -100,13 +116,13 @@ def epg(
     E2 = np.exp(-TR / T2)  # Eq.[23] in EPG-R
 
     # Generate state matrices Omega before and after RF: Eq.[26] in EPG-R
-    Omega_preRF = np.zeros(3, Np1)
-    Omega_postRF = np.zeros(3, N)
+    Omega_preRF = np.zeros((3, Np1), dtype=complex)
+    Omega_postRF = np.zeros((3, N), dtype=complex)
 
     # Generate state evolution matrices Xi_F and Xi_Z: Eqs.[27] in EPG-R
     # Here, they will (only) contain all post-RF states == output variables
-    Xi_F_out = np.zeros(2 * N - 1, N)  # Xi_F with F+ and F- states, Eq.[27a]
-    Xi_Z_out = np.zeros(N, N)          # Xi_Z with Z states, Eq.[27b]
+    Xi_F_out = np.zeros((2 * N - 1, N), dtype=complex)  # Xi_F with F+ and F- states, Eq.[27a]
+    Xi_Z_out = np.zeros((N, N), dtype=complex)          # Xi_Z with Z states, Eq.[27b]
 
     # Starting with equilibrium magnetization M0=1 for t<0
     # You could change that !
@@ -121,9 +137,9 @@ def epg(
         # Eq.[15] or Eq.[18] in EPG-R
         # Since this generic SSFP example may change RF flip angle and/or
         # phase, the T matrix has to be updated within the RF pulse loop:
-        T = np.zeros(3, 3)
-        T[0, 0] = np.cos(fa(pn) / 2) ** 2
-        T[0, 1] = np.exp(+2j * phi[pn]) * np.sin(fa(pn) / 2) ** 2
+        T = np.zeros((3, 3), dtype=complex)
+        T[0, 0] = np.cos(fa[pn] / 2) ** 2
+        T[0, 1] = np.exp(+2j * phi[pn]) * np.sin(fa[pn] / 2) ** 2
         T[0, 2] = -1.0j * np.exp(+1j * phi[pn]) * np.sin(fa[pn])
         T[1, 0] = np.exp(-2j * phi[pn]) * np.sin(fa[pn] / 2) ** 2
         T[1, 1] = np.cos(fa[pn] / 2) ** 2
@@ -143,16 +159,17 @@ def epg(
             k = range(pn)  # "k loop index" with limit pn
         else:
             k = range(maxstate)
+        k = np.array(list(k), dtype=int)
 
         # T matrix operator: RF pulse acting, mixing of F+, F-, and Z states
         # Expand T matrix relations from Eq.[15] or Eq.[18] in EPG-R
-        Omega_postRF[:, k] = T * Omega_preRF[:, k]  # matrix product!
+        Omega_postRF[:, k] = np.matmul(T, Omega_preRF[:, k])  # matrix product!
 
         # Store these post-RF states of the current Omega state matrix in
         # the Xi state evolution matrices
-        Xi_F_out[Np1 - k, pn] = Omega_postRF[0, k]
+        Xi_F_out[Np1 - k - 2, pn] = Omega_postRF[0, k]
         Xi_F_out[Nm1 + k[1:], pn] = np.conj(Omega_postRF[1, k[1:]])
-        Xi_Z_out[Np1 - k, pn] = Omega_postRF[2, k]
+        Xi_Z_out[Np1 - k - 2, pn] = Omega_postRF[2, k]
 
         # E matrix operator: Experienced relaxation from the states
         # until the next TR.
@@ -174,14 +191,61 @@ def epg(
 
     # Output: "make nice zeros"
     # Erase some float point accuracy errors
-    Xi_F_out[np.abs(Xi_F_out) < 1e-5] = 0
-    Xi_Z_out[np.abs(Xi_Z_out) < 1e-5] = 0
+    Xi_F_out[np.abs(Xi_F_out) < 1e-8] = 0
+    Xi_Z_out[np.abs(Xi_Z_out) < 1e-8] = 0
 
     # Output: define "echoes" separately
-    F0_vector_out = Xi_F_out[N, :]
+    F0_vector_out = Xi_F_out[N - 1, :]
 
     # Subtract the RF phase from the simulations for each readout
     if rf_phase_mode == 1:
         F0_vector_out = F0_vector_out * np.exp(-1j * phi)
 
     return F0_vector_out, Xi_F_out, Xi_Z_out
+
+
+def example(plot: bool = True):
+    """Example function for using epg()"""
+
+    # Define parameters
+    T1 = .583       # T1 relaxation time of the spin [s]
+    T2 = 0.055      # T2 relaxation time of the spin [s]
+    B1 = 1          # B1 field of the spin [-]
+    fa = 20         # Flip angle of the sequence [deg] (Can also be an array)
+    Nfa = 500       # Number of flip angles to achieve a steady state [-]
+    tr = 10E-03     # Repetition time [s]
+    spoil = 1       # 0 = balanced, 1 = spoiled
+
+    # Perform EPG Simulation
+    F0, Xi_F, Xi_Z = epg(Nfa, fa, tr, T1, T2, complex(1, 0), spoil)
+
+    # Plot results (if applicable)
+    if plot:
+        plt.figure()  # Create figure
+
+        plt.subplot(2, 2, 1)  # Absolute signal
+        plt.plot(range(len(F0)), np.abs(F0))
+        plt.title("Abs(signal)")
+        plt.xlabel("Pulse n")
+        plt.ylabel("Signal")
+        plt.subplot(2, 2, 2)  # Real part
+        plt.plot(range(len(F0)), np.real(F0))
+        plt.title("Real(signal)")
+        plt.xlabel("Pulse n")
+        plt.ylabel("Signal")
+        plt.subplot(2, 2, 3)  # Imaginary part
+        plt.plot(range(len(F0)), np.imag(F0))
+        plt.title("Imag(signal)")
+        plt.xlabel("Pulse n")
+        plt.ylabel("Signal")
+        plt.subplot(2, 2, 4)  # Angle
+        plt.plot(range(len(F0)), np.angle(F0))
+        plt.title("Angle(signal)")
+        plt.xlabel("Pulse n")
+        plt.ylabel("Angle [rad]")
+
+        plt.show()  # Show figure
+
+
+if __name__ == "__main__":
+    example()
