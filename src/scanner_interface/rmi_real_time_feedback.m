7b87f95efc03e15220e9edb57ccc97d266547453
shaverschuren <s.h.a.verschuren@student.tue.nl>
2021-11-01 23:12:03 +0100

    Added matlab-python interface to scanner interface

--- src/scanner_interface/rmi_real_time_feedback.m
+++ src/scanner_interface/rmi_real_time_feedback.m
@@ -5,9 +5,9 @@
 % Some setup parameters
 clearvars;clc;
 cd('/nfs/arch11/researchData/USER/tbruijne/Projects_Main/ReconSocket/recon-socket-repo/recon-socket/matlab_scripts/caspr/utils')
-% run caspr_machine_setup.m
-% par = check_default_values(par);
-% par.machine_id = "rtrabbit";
+run caspr_machine_setup.m
+par = check_default_values(par);
+par.machine_id = "rtrabbit";
 text_file_loc = '/nfs/rtsan01/RT-Temp/TomBruijnen/machine_flip_angles.txt';
 img_file_loc = '/nfs/rtsan01/RT-Temp/TomBruijnen/img.h5';
 
@@ -27,16 +27,27 @@ while 1
     % Read current image
     img = ReconSocketWrapper(2);
     if ~isempty(img)
+        % Set up image
         img = reshape(img(1).real,img(1).size_dim');
+        % Store image in img_store
         if isempty(img_store)
             img_store = img;
         else
             img_store(:,:,end+1) = img;
         end
-        % Store image (ready for python RL model)
-        h5write(data_loc,'/img',img_store);
+
+        % Write image to data drop (for Python pickup)
+        h5write(data_loc,'/img',img);
+        % Create file to signal completion to Python
+        fid = fopen(signal_file_loc, 'w');
+        fclose(fid);
+        disp(['RMI: Passed image...']);
+        % Wait for Python to respond
+        while exist(signal_file_loc)
+            pause(0.1);
+        end
     end
 
-    disp(['RMI: waiting...']);pause(1);
+    disp(['RMI: waiting...']);pause(0.1);
            
 end
