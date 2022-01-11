7c31dad7358977b44fc9d6cb8340a822e2fdfb80
shaverschuren <s.h.a.verschuren@student.tue.nl>
2022-01-11 15:00:42 +0100

    Implemented callback for python remote

--- src/scanner_interface/python/rmqReceiveImage.py
+++ src/scanner_interface/python/rmqReceiveImage.py
@@ -6,12 +6,17 @@ import os
 import sys
 
 dir = os.path.dirname(os.path.realpath(__file__))
+src = os.path.dirname(os.path.dirname(dir))
+root = os.path.dirname(src)
 if dir not in sys.path: sys.path.append(dir)
+if src not in sys.path: sys.path.append(src)
+if root not in sys.path: sys.path.append(root)
 
 # File-specific imports
 import argparse                 # noqa: E402
 import numpy as np              # noqa: E402
 import json                     # noqa: E402
+import h5py                     # noqa: E402
 import pika                     # noqa: E402
 import dataobject_pb2 as pb     # noqa: E402 (pb = protobuf)
 
@@ -41,6 +46,20 @@ def get_args():
     return args
 
 
+def read_config():
+    """Read info from config file for scanner interaction"""
+
+    # Define config path
+    global root
+    config_path = os.path.join(root, "config.json")
+
+    # Read config file
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+
+    return config
+
+
 def callback_image(ch, method, properties, body):
     """Callback function for receiving an image"""
 
@@ -49,27 +68,56 @@ def callback_image(ch, method, properties, body):
     img_number += 1
     # Print some info
     print("Image number", img_number, "received.")
+
     # Store received data in proper format (np array)
     dataobject = pb.DataObject()
     dataobject.ParseFromString(body)
     image_data = dataobject.image
     image_data = np.reshape(image_data, dataobject.size_dim)
-    # TODO: Store image in proper file (might do this in memory later)
+
+    # Store image in proper files (might do this in memory later)
+    # We write it in the img_data file (used as most recent image),
+    # as well as in the image store folder (mostly for debugging)
+    global config
+    data_paths = [
+        # Most recent image
+        config['data_loc'],
+        # Stored images
+        os.path.join(config['data_store_loc'], f"img_{img_number}.h5")
+    ]
+    for data_path in data_paths:
+        # Write data to locked h5 file
+        hf = h5py.File(data_path + ".lck", 'w')
+        hf.create_dataset('/img', data=np.array(image_data, dtype=np.float16))
+        hf.close()
+        # Move data file to actual reading location
+        os.system(f"mv {data_path + '.lck'} {data_path}")
 
 
 def rmq_setup_channel(machine_id):
     """Setup connection to remote device (rtrabbit)"""
 
     # Fill in credentials
-    cred = pika.PlainCredentials('mquser', 'mquser')
+    global config
+    cred = pika.PlainCredentials(*config['remote_credentials'])
     # Establish connection
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=machine_id,credentials=cred))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host=machine_id, credentials=cred
+    ))
 
-    # Open channel and bind queues + callback  
+    # Open channel and bind queues + callback
     channel = connection.channel()
     channel.queue_declare(queue='reconsocket_image_dev')
-    channel.queue_bind(exchange='reconsocket_image_dev', queue='reconsocket_image_dev',routing_key='reconsocket.image')
-    channel.basic_consume(queue='reconsocket_image_dev', on_message_callback=callback_image, auto_ack=True)
+    channel.queue_bind(
+        exchange='reconsocket_image_dev',
+        queue='reconsocket_image_dev',
+        routing_key='reconsocket.image'
+    )
+    channel.basic_consume(
+        queue='reconsocket_image_dev',
+        on_message_callback=callback_image,
+        auto_ack=True
+    )
 
     # Return the channel
     return channel
@@ -94,8 +142,12 @@ if __name__ == "__main__":
 
     # Define starting image number
     img_number = 0
+
+    # Read config data
+    config = read_config()
     # Get arguments
     args = get_args()
+
     # Setup channel to remote computer
     channel = rmq_setup_channel(args.m)
 
