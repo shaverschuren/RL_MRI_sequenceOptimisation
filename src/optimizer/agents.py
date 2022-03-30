b597406bedeed8535e76fb38ecc9794c55bc05c8
Sjors Verschuren <s.h.a.verschuren@student.tue.nl>
2022-03-30 23:00:46 +0200

    Experimenting with gradient flow through time

--- src/optimizer/agents.py
+++ src/optimizer/agents.py
@@ -14,6 +14,37 @@ from torch import optim
 import torch.nn.functional as F
 from torch.autograd import Variable
 from optimizer import environments, models
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
+
+def plot_grad_flow(named_parameters):
+    '''Plots the gradients flowing through different layers in the net during training.
+    Can be used for checking for possible gradient vanishing / exploding problems.
+    
+    Usage: Plug this function in Trainer class after loss.backwards() as 
+    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow'''
+    ave_grads = []
+    max_grads= []
+    layers = []
+    for n, p in named_parameters:
+        if(p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+            max_grads.append(p.grad.abs().max())
+    plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
+    plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, lw=2, color="k" )
+    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(left=0, right=len(ave_grads))
+    plt.ylim(bottom = -0.001, top=0.02) # zoom in on the lower gradient regions
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.legend([Line2D([0], [0], color="c", lw=4),
+                Line2D([0], [0], color="b", lw=4),
+                Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])
 
 
 class DQNAgent(object):
@@ -737,7 +768,7 @@ class RDPGAgent(object):
             # Compute critic loss
             critic_loss = self.critic_criterion(Q, Q_target)
 
-            # Compute actor loss
+            # Compute actor loss TODO: Maybe use target critic for this??
             policy_loss = -self.critic(
                 torch.cat([states, policy], 1),
                 hidden_critic[t]
@@ -752,32 +783,38 @@ class RDPGAgent(object):
                 critic_loss_total = critic_loss
 
             # Update hidden states
-            hidden_critic.append(
-                (hidden_critic_1[0].detach(), hidden_critic_1[1].detach())
-            )
-            hidden_actor.append(
-                (hidden_actor_1[0].detach(), hidden_actor_1[1].detach())
-            )
-            hidden_critic_target.append((
-                hidden_critic_target_1[0].detach(),
-                hidden_critic_target_1[1].detach()
-            ))
-            hidden_actor_target.append((
-                hidden_actor_target_1[0].detach(),
-                hidden_actor_target_1[1].detach()
-            ))
+            # hidden_critic.append(
+            #     (hidden_critic_1[0].detach(), hidden_critic_1[1].detach())
+            # )
+            # hidden_actor.append(
+            #     (hidden_actor_1[0].detach(), hidden_actor_1[1].detach())
+            # )
+            # hidden_critic_target.append((
+            #     hidden_critic_target_1[0].detach(),
+            #     hidden_critic_target_1[1].detach()
+            # ))
+            # hidden_actor_target.append((
+            #     hidden_actor_target_1[0].detach(),
+            #     hidden_actor_target_1[1].detach()
+            # ))
+            hidden_critic.append(hidden_critic_1)
+            hidden_actor.append(hidden_actor_1)
+            hidden_critic_target.append(hidden_critic_target_1)
+            hidden_actor_target.append(hidden_actor_target_1)
 
             # Detach hidden states
-            self.detach_hidden()
+            # self.detach_hidden()
 
-            # Update networks
-            self.actor_optimizer.zero_grad()
-            policy_loss.backward()
-            self.actor_optimizer.step()
+        # Update networks
+        self.actor_optimizer.zero_grad()
+        policy_loss_total.backward(retain_graph=True)
+        # plot_grad_flow(self.actor.named_parameters())
+        self.actor_optimizer.step()
 
-            self.critic_optimizer.zero_grad()
-            critic_loss.backward()
-            self.critic_optimizer.step()
+        self.critic_optimizer.zero_grad()
+        critic_loss_total.backward()
+        # plot_grad_flow(self.critic.named_parameters())
+        self.critic_optimizer.step()
 
         # Update target networks (lagging weights)
         for target_param, param in zip(
