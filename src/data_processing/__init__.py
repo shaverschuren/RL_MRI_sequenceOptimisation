b4e9529b4fe2d14677c4b8cd5c473fb35d2999cc
Sjors Verschuren <s.h.a.verschuren@student.tue.nl>
2022-06-14 00:26:38 +0200

    Added data processing module

---
+++ src/data_processing/__init__.py
@@ -0,0 +1,6 @@
+"""Module containing data processing scripts
+
+These are specific for this project, so not likely
+useful for further use. Will store them in this repo
+for version control though.
+"""
