754a1f40f846852da6cbbf2da113a54f6a697b5a
Sjors Verschuren <s.h.a.verschuren@student.tue.nl>
2022-06-30 15:31:30 +0200

    Hyperparameter tweak

--- src/optimizer/agents.py
+++ src/optimizer/agents.py
@@ -733,31 +733,31 @@ class RDPGAgent(object):
             # For single fa case, only actor/critic optimizers
             self.actor_optimizer = optim.Adam(
                 self.actor.parameters(),
-                lr=self.alpha_actor, weight_decay=1e-2
+                lr=self.alpha_actor, weight_decay=1e-3
             )
             self.critic_optimizer = optim.Adam(
                 self.critic.parameters(),
-                lr=self.alpha_critic, weight_decay=1e-2
+                lr=self.alpha_critic, weight_decay=1e-3
             )
         else:
             # For full pulse train case, also add cnr predictor optimizer
             self.cnr_optimizer = optim.Adam(
                 self.cnr_predictor.parameters(),
-                lr=self.alpha_cnr_predictor, weight_decay=1e-3
+                lr=self.alpha_cnr_predictor, weight_decay=1e-4
             )
             self.actor_optimizer = optim.Adam(
                 [
                     *self.actor.stack_kspace.parameters(),   # type: ignore
                     *self.actor.stack_theta.parameters(),    # type: ignore
                     *self.actor.stack_rnn.parameters()       # type: ignore
-                ], lr=self.alpha_actor, weight_decay=1e-3
+                ], lr=self.alpha_actor, weight_decay=1e-4
             )
             self.critic_optimizer = optim.Adam(
                 [
                     *self.critic.stack_kspace.parameters(),  # type: ignore
                     *self.critic.stack_theta.parameters(),   # type: ignore
                     *self.critic.stack_rnn.parameters()      # type: ignore
-                ], lr=self.alpha_critic, weight_decay=1e-3
+                ], lr=self.alpha_critic, weight_decay=1e-4
             )
 
         # Setup criterions
