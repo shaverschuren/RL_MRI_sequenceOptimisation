d54b754f90d53e824839aa1fb4c2645e979a0cbc
Sjors Verschuren <s.h.a.verschuren@student.tue.nl>
2022-08-11 12:26:57 +0200

    Bugfix regarding hidden state size

--- src/optimizer/models.py
+++ src/optimizer/models.py
@@ -630,8 +630,8 @@ class CombinedModel_PulsetrainOptimizer(nn.Module):
         """Reset hidden state of the lstm module"""
 
         self.hx = Variable(
-            torch.zeros(3, batch_size, self.hidden_size)
+            torch.zeros(1, batch_size, self.hidden_size)
         ).to(self.device)
         self.cx = Variable(
-            torch.zeros(3, batch_size, self.hidden_size)
+            torch.zeros(1, batch_size, self.hidden_size)
         ).to(self.device)
