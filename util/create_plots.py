68e9a741661ae15bbc2476ed2e47586e2ea99b95
Sjors Verschuren <s.h.a.verschuren@student.tue.nl>
2022-08-12 20:40:53 +0200

    Added SNR images

--- util/create_plots.py
+++ util/create_plots.py
@@ -4,6 +4,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+import pickle
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tb2csv
@@ -42,7 +43,7 @@ def plot_a_snr_train_loss(dirs):
     plt.ylabel(" ")
     plt.yticks([0], [0])
     plt.xlabel("Episodes")
-    plt.xticks([0, len(df_a_snr_train) - 400], [0, len(df_a_snr_train)])
+    plt.xticks([0, len(df_a_snr_train) - 400], [0, "10k"])
 
     ax1 = sns.lineplot(x="step", y="policy_loss_ave", data=df_a_snr_train, label="Policy loss")
     ax2 = sns.lineplot(x="step", y="critic_loss_ave", data=df_a_snr_train, label="Critic loss")
@@ -94,7 +95,7 @@ def plot_a_cnr_train_loss(dirs):
     plt.ylabel(" ")
     plt.yticks([0], [0])
     plt.xlabel("Episodes")
-    plt.xticks([0, len(df_a_cnr_train) - 400], [0, len(df_a_cnr_train)])
+    plt.xticks([0, len(df_a_cnr_train) - 400], [0, "10k"])
 
     ax1 = sns.lineplot(x="step", y="policy_loss_ave", data=df_a_cnr_train, label="Policy loss")
     ax2 = sns.lineplot(x="step", y="critic_loss_ave", data=df_a_cnr_train, label="Critic loss")
@@ -110,7 +111,6 @@ def plot_a_cnr_train_loss(dirs):
 
 
     # Create and store figure
-    # fig = a_snr_train.get_figure()
     fig.savefig(os.path.join(dirs["to_dir"], "a_cnr_train.png"))
 
 
@@ -150,22 +150,115 @@ def plot_a_metrics_test(dirs):
     fig.savefig(os.path.join(dirs["to_dir"], "a_test_metric.png"))
 
 
+def plot_a_snr_images(dirs):
+    """Plot series of images for illustrative purposes (experiment A)"""
+
+    # Create new dir if applicable
+    to_img_dir = os.path.join(dirs["to_dir"], "a_snr_test", "imgs")
+    if not os.path.isdir(to_img_dir): os.mkdir(to_img_dir)
+
+    # Get data
+    with open(os.path.join(dirs["to_dir"], "a_snr_test", "img.pickle"), 'rb') as f:
+        imgs_dict = pickle.load(f)
+
+    # Loop over tags
+    for tag in imgs_dict.keys():
+        # Retrieve name of tag
+        name = tag[20:]
+
+        # Retrieve image series and steps
+        imgs = [row[0] for row in imgs_dict[tag]]
+        steps = [row[1] for row in imgs_dict[tag]]
+
+        # Concatenate images to side-by-side
+        if len(imgs) > 1:
+            img_concat = np.concatenate([
+                np.concatenate(imgs[:len(imgs) // 2], axis=1),
+                np.concatenate(imgs[len(imgs) // 2 : -1], axis=1)
+            ], axis=0
+            )
+        else:
+            img_concat = imgs[0]
+
+        # Yield slightly more contrast intra- and inter-phantom via windowing
+        img_concat = np.array(img_concat, dtype=np.float64)
+        img_concat -= np.percentile(img_concat, 50.)
+        img_concat *= 256. / np.percentile(img_concat, 99.)
+        img_concat = np.array(np.clip(img_concat, 0, 255), dtype=np.uint8)
+
+        # Store image row
+        plt.imshow(img_concat, cmap="gray")
+        plt.axis('off')
+        plt.savefig(os.path.join(to_img_dir, f"{name}.png"), dpi=500)
+        plt.close()
+
+
+def plot_b_train_loss(dirs):
+    """Plot loss curve for experiment A (CNR)"""
+
+    # Retrieve appropriate dataframe
+    df_b_train = pd.read_csv(
+        os.path.join(dirs["to_dir"], "b_train", "train_episodes.csv")
+    )
+
+    # Calculate rolling averages + CI bars
+    df_b_train['policy_loss_ave'] = df_b_train.policy_loss.rolling(100).mean().shift(-99)
+    df_b_train['critic_loss_ave'] = df_b_train.critic_loss.rolling(100).mean().shift(-99)
+    df_b_train["reward"] = df_b_train["reward"] / 10.  # Average reward per step
+    df_b_train['reward_ave'] = df_b_train.reward.rolling(100).mean().shift(-99)
+
+    df_b_train["policy_loss_res"] = abs(df_b_train["policy_loss"] - df_b_train["policy_loss_ave"]).rolling(50).mean().shift(-49)
+    policy_loss_CI_max = df_b_train['policy_loss_ave'] + df_b_train["policy_loss_res"]
+    policy_loss_CI_min = df_b_train['policy_loss_ave'] - df_b_train["policy_loss_res"]
+
+    df_b_train["critic_loss_res"] = abs(df_b_train["critic_loss"] - df_b_train["critic_loss_ave"]).rolling(50).mean().shift(-49)
+    critic_loss_CI_max = df_b_train['critic_loss_ave'] + df_b_train["critic_loss_res"]
+    critic_loss_CI_min = df_b_train['critic_loss_ave'] - df_b_train["critic_loss_res"]
+
+    df_b_train["reward_res"] = abs(df_b_train["reward"] - df_b_train["reward_ave"]).rolling(200).mean().shift(-199)
+    reward_CI_max = (df_b_train['reward_ave'] + df_b_train["reward_res"]).rolling(100).mean().shift(-99)
+    reward_CI_min = (df_b_train['reward_ave'] - df_b_train["reward_res"]).rolling(100).mean().shift(-99)
+
+    # Create plots
+    fig, ax = plt.subplots()
+    plt.xlim(0, len(df_b_train) - 400)
+    plt.ylim(-12, 22.5)
+    plt.ylabel(" ")
+    plt.yticks([0], [0])
+    plt.xlabel("Episodes")
+    plt.xticks([0, len(df_b_train) - 400], [0, len(df_b_train)])
+
+    ax1 = sns.lineplot(x="step", y="policy_loss_ave", data=df_b_train, label="Policy loss")
+    ax2 = sns.lineplot(x="step", y="critic_loss_ave", data=df_b_train, label="Critic loss")
+    ax3 = sns.lineplot(x="step", y="reward_ave", data=df_b_train, label="Reward")
+
+    plt.fill_between(df_b_train.step, policy_loss_CI_min, policy_loss_CI_max, alpha=.3)
+    plt.fill_between(df_b_train.step, critic_loss_CI_min, critic_loss_CI_max, alpha=.3)
+    plt.fill_between(df_b_train.step, reward_CI_min, reward_CI_max, alpha=.3)
+
+    plt.axhline(y=0., color='k', linestyle='--', linewidth=.5)
+
+    plt.legend(frameon=False)
+
+    # Create and store figure
+    fig.savefig(os.path.join(dirs["to_dir"], "b_train.png"))
+
+
 def generate_plots(dirs: dict[str, str]):
     """Generate plots"""
 
-    # Training curves A - SNR
-    plot_a_snr_train_loss(dirs)
+    # Training curves experiment A - SNR & CNR
+    # plot_a_snr_train_loss(dirs)
+    # plot_a_cnr_train_loss(dirs)
 
-    # Training curves A - CNR
-    plot_a_cnr_train_loss(dirs)
+    # # Test curve experiment A
+    # plot_a_metrics_test(dirs)
 
-    # Test curve A
-    plot_a_metrics_test(dirs)
+    # Images experiment A - SNR & CNR
+    plot_a_snr_images(dirs)
 
     # Training curve B
-    # df_b_train = pd.read_csv(
-    #     os.path.join(dirs["to_dir"], "b_train", "train_episodes.csv")
-    # )
+    # plot_b_train_loss(dirs)
 
     # Test curves A - SNR
 
